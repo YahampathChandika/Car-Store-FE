83d54525975e3d61af213b2aa3f1e3afc68b94dd
ridmijayasekara2713 <ridmidiljayasekara2713@gmail.com>
2024-10-14 12:40:50 +0530

    Contact Section

---
+++ src/components/home/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "../../assets/css/Contact.css"
+export default function Contact() {
+  return (
+    <div>
+      fgh
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import '../assets/css/Home.css';
-
+import "../assets/css/Home.css";
 import Hero from "../components/home/Hero";
 import Navbar from "../components/common/Navbar";
+import Contact from "../components/home/Contact";
 
 export default function Home() {
   return (
     <div className="home-container">
       <Navbar />
       <Hero />
+      <Contact />
     </div>
   );
 }
