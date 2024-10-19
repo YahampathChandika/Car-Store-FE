627cef4c5346db69d4e02ce065c88c7489edf141
ridmijayasekara2713 <ridmidiljayasekara2713@gmail.com>
2024-10-19 20:23:08 +0530

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../assets/css/Home.css";
 import Hero from "../components/home/Hero";
 import Navbar from "../components/common/Navbar";
+import Latest from "../components/home/Latest";
+import Contact from "../components/home/Contact";
 
 export default function Home() {
   return (
     <div className="home-container">
       <Navbar />
       <Hero />
+      <Latest />
+      <Contact />
     </div>
   );
 }
